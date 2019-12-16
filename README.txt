cc1e2fd15c7ecab97cf2a25ac75c9360efcc7b7d
Simon <simonhehehe@gmail.com>
2019-12-16 18:19:36 +0100

    shared canvas docker

---
+++ README.txt
@@ -0,0 +1,5 @@
+canvas körs på en färdiggjord image...
+skapa inuti SharedCanvas först med:
+docker build -t canvas .
+sedan docker-compose som vanligt
+
--- SharedCanvas/Server.js
+++ SharedCanvas/Server.js
@@ -2,18 +2,48 @@ var express = require('express');
 
 var app = express();
 var server = app.listen(3000);
-
 app.use(express.static('public'));
 
 var socket = require('socket.io');
 var io = socket(server);
 io.sockets.on('connection', newConnection);
 
+var fs = require('fs');
+
 function newConnection(socket){
   console.log("New connection: " + socket.id);
   socket.on('coordinate', coordinateMsg);
+  socket.on('savedata', saveToFile);
+  socket.on('loaddata', loadData);
   function coordinateMsg(coordinate){
     socket.broadcast.emit('coordinate', coordinate)
-      console.log(coordinate);
+  //    console.log(coordinate);
+  }
+  function saveToFile(data, filename){
+    console.log("save to file");
+    console.log(data);
+    console.log(filename);
+    var jsonData = JSON.stringify(data);
+    
+    fs.writeFile(filename + ".json", jsonData, uploadFinished);
+   
+    function uploadFinished(err){
+
+      console.log("Uploaded file");
+    }
+  }
+
+  function loadData(filename){
+    console.log("Opening file: " + filename + ".json");
+    try{
+      var jsonData = fs.readFileSync(filename + ".json");
+    }catch(err){
+      console.log("No such file exists");
+      return;
+    }
+    var data = JSON.parse(jsonData);
+    console.log(data);
+    socket.emit('loaddata', data);
   }
 }
+
