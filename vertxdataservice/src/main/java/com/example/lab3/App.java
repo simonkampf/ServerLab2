6da7f3e0869c40d522ff4816a719cb8e648c52de
Alexander Evert <Alex@Macintosh.lan>
2019-12-19 10:58:05 +0100

    vertxadded

---
+++ vertxdataservice/src/main/java/com/example/lab3/App.java
@@ -0,0 +1,15 @@
+package com.example.lab3;
+
+
+import io.vertx.core.Vertx;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new PlotVerticle());
+
+    }
+}
